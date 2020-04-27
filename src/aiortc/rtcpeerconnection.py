f8cc659df8b29c28ad525d7200b3567fdee2e11d
Jeremy Lainé <jeremy.laine@m4x.org>
2020-04-27 22:47:44 +0200

    [breaking] make RTCPeerConnection.addIceCandidate a coroutine
    This will be needed to support mDNS candidates. It also matches the
    JavaScript WebRTC API more closely.

--- src/aiortc/rtcpeerconnection.py
+++ src/aiortc/rtcpeerconnection.py
@@ -149,13 +149,13 @@ def add_transport_description(
         media.dtls.fingerprints = dtlsTransport.getLocalParameters().fingerprints
 
 
-def add_remote_candidates(
+async def add_remote_candidates(
     iceTransport: RTCIceTransport, media: sdp.MediaDescription
 ) -> None:
     for candidate in media.ice_candidates:
-        iceTransport.addRemoteCandidate(candidate)
+        await iceTransport.addRemoteCandidate(candidate)
     if media.ice_candidates_complete:
-        iceTransport.addRemoteCandidate(None)
+        await iceTransport.addRemoteCandidate(None)
 
 
 def allocate_mid(mids: Set[str]) -> str:
@@ -335,7 +335,7 @@ class RTCPeerConnection(AsyncIOEventEmitter):
     def signalingState(self):
         return self.__signalingState
 
-    def addIceCandidate(self, candidate: RTCIceCandidate) -> None:
+    async def addIceCandidate(self, candidate: RTCIceCandidate) -> None:
         """
         Add a new :class:`RTCIceCandidate` received from the remote peer.
 
@@ -349,7 +349,7 @@ class RTCPeerConnection(AsyncIOEventEmitter):
         for transceiver in self.__transceivers:
             if candidate.sdpMid == transceiver.mid and not transceiver._bundled:
                 iceTransport = transceiver._transport.transport
-                iceTransport.addRemoteCandidate(candidate)
+                await iceTransport.addRemoteCandidate(candidate)
                 return
 
         if (
@@ -358,7 +358,7 @@ class RTCPeerConnection(AsyncIOEventEmitter):
             and not self.__sctp._bundled
         ):
             iceTransport = self.__sctp.transport.transport
-            iceTransport.addRemoteCandidate(candidate)
+            await iceTransport.addRemoteCandidate(candidate)
 
     def addTrack(self, track: MediaStreamTrack) -> RTCRtpSender:
         """
@@ -835,7 +835,7 @@ class RTCPeerConnection(AsyncIOEventEmitter):
             if dtlsTransport is not None:
                 # add ICE candidates
                 iceTransport = dtlsTransport.transport
-                add_remote_candidates(iceTransport, media)
+                await add_remote_candidates(iceTransport, media)
 
                 # set ICE role
                 if description.type == "offer" and not iceTransport._role_set:
